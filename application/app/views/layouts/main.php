1c6011d1f424eff5af021b992ab01688c2f7ee01
mdmunir <misbahuldmunir@gmail.com>
2014-12-23 13:49:30 +0700

    add easyui

--- application/app/views/layouts/main.php
+++ application/app/views/layouts/main.php
@@ -1,19 +1,15 @@
 <?php
 
 use yii\helpers\Html;
-use yii\helpers\Inflector;
-use yii\widgets\Breadcrumbs;
 
-/**
- * @var \yii\web\View $this
- * @var string $content
- */
+/* @var $this \yii\web\View */
+/* @var $content string */
 
-$asset = app\assets\AppAsset::register($this);
+\app\assets\EasyuiAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" manifest="<?= isset($this->context->manifestFile) ? $this->context->manifestFile : '' ?>">
+<html lang="<?= Yii::$app->language ?>">
     <head>
         <meta charset="<?= Yii::$app->charset ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,51 +18,48 @@ $asset = app\assets\AppAsset::register($this);
         <?php $this->head() ?>
     </head>
     <?php $this->beginBody() ?>
-    <body class="skin-blue fixed">
-        <header class="header">
-            <?php echo $this->render('heading', ['baseurl' => $asset->baseUrl]); ?>
-        </header>
-        <div class="wrapper row-offcanvas row-offcanvas-left">
-            <aside class="right-side">
-                <section class="content-header">
-                    <h1>
-                        <?= '&nbsp;' . Html::encode($this->title) ?>
-                        <small><?php echo \Yii::$app->controller->id . '-' . \Yii::$app->controller->action->id; ?></small>
-                    </h1>
-                    <?php
-                    $breadcrumbs = isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [];
-                    foreach (Yii::$app->controller->modules as $module) {
-                        if ($module != Yii::$app) {
-                            array_unshift($breadcrumbs, ['label' => Inflector::camel2words($module->id), 'url' => ['/' . $module->uniqueId]]);
-                        }
-                    }
-                    ?>
-                    <?=
-                    Breadcrumbs::widget([
-                        'tag'=>'ol',
-                        'encodeLabels'=>false,
-                        'homeLink'=>['label'=>'<i class="fa fa-dashboard"></i> Home/Dashboard','url'=>['/site/index']],
-                        'links' => $breadcrumbs,
-                    ])
-                    ?>
-                </section>
-                <section class="content">
-                    <?= $content ?>
-                </section>
-            </aside>
-            <aside class="left-side sidebar-offcanvas">
-                <?php echo $this->render('sidebar', ['baseurl' => $asset->baseUrl]); ?>
-            </aside>
-        </div>
+    <body class="easyui-layout" style="text-align:left">
+        <div region="north" border="false" style="background:#666;text-align:center">
+            <div id="header-inner">
+                <table cellpadding="0" cellspacing="0" style="width:100%;">
+                    <tr>
+                        <td rowspan="2" style="width:20px;">
+                        </td>
+                        <td style="height:52px;">
+                            <div style="color:#fff;font-size:22px;font-weight:bold;">
+                                <a href="/index.php" style="color:#fff;font-size:22px;font-weight:bold;text-decoration:none">jQuery EasyUI</a>
+                            </div>
+                            <div style="color:#fff">
+                                <a href="/index.php" style="color:#fff;text-decoration:none">easyui helps you build your web pages easily!</a>
+                            </div>
+                        </td>
+                        <td style="padding-right:5px;text-align:right;vertical-align:bottom;">
+                            <div id="topmenu">
+                                <a href="/index.php">Home</a>
+                                <a href="/demo/main/index.php">Demo</a>
+                                <a href="/tutorial/index.php">Tutorial</a>
+                                <a href="/documentation/index.php">Documentation</a>
+                                <a href="/download/index.php">Download</a>
+                                <a href="/extension/index.php">Extension</a>
+                                <a href="/contact.php">Contact</a>
+                                <a href="/forum/index.php">Forum</a>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
 
-        <!--        <footer class="footer">
-                    <div class="container">
-                        <p class="pull-left">&copy; My Company <?= ''//date('Y')              ?></p>
-                        <p class="pull-right"><?= ''//Yii::powered()              ?></p>
-                    </div>
-                </footer>-->
-    </div>
-    <?php $this->endBody() ?>
-</body>
+        </div>
+        <div region="west" split="true" border="false" title=" " style="width:250px;padding:5px;">
+            <ul class="easyui-tree">
+            </ul>
+        </div>
+        <div region="center" border="false">
+            <div id="main-content">
+                <?= $content ?>
+            </div>
+        </div>
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
